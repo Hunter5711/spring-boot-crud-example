e333764b66b814f7f0adc30ca7397110be84cdbb
hunter5711 <davidwarner191197@gmail.com>
2022-06-15 17:20:30 +0530

    added getter and setter for User class

--- src/main/java/com/javatechie/crud/example/entity/User.java
+++ src/main/java/com/javatechie/crud/example/entity/User.java
@@ -4,4 +4,27 @@ public class User {
 
     // Adding user entity class for next feature.
     //just to test commmit msg
+
+    String name ;
+    int id;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
